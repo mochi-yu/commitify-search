b643d427f0ccbf2e5261d0dbb5fba911b5581caa
mochi-yu <mochi.2225.it@gmail.com>
2023-12-14 01:59:34 +0900

    fix: 動作確認用のコードを削除

--- api/run_query.py
+++
@@ -1,9 +0,0 @@
-from qdrant_client import QdrantClient
-
-client = QdrantClient(host="qdrant-db", port=6333)
-
-search_result = client.search(
-    collection_name="test_collection", query_vector=[0.2, 0.1, 0.9, 0.7], limit=3
-)
-
-print(search_result)
